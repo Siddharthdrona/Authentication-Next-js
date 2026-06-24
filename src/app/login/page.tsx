f0c54a5d1aa21fdc14701b8b2efc7a4fbc1280fe
Siddharth Dronachary <siddudrona@gmail.com>
2026-06-24 21:50:32 +0530

    refactor(login): clean up code formatting

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,6 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import toast from "react-hot-toast";
-
 import Image from "next/image";
 
 export default function LoginPage() {
@@ -23,7 +22,7 @@ export default function LoginPage() {
       const response = await axios.post("/api/users/login", user);
       console.log("login sucess", response.data);
       toast.success("Login sucess");
-      router.push("/profile"); // IMP step
+      router.push("/profile");
     } catch (error: any) {
       console.log("Login failed", error.response?.data);
       toast.error(error.response?.data?.error || error.message);
@@ -45,37 +44,17 @@ export default function LoginPage() {
       {/* background glow */}
       <div className="absolute w-96 h-96 bg-blue-500/30 rounded-full blur-3xl"></div>
 
-      <div
-        className="relative w-full max-w-md
-    bg-white/10 backdrop-blur-xl
-    border border-white/20
-    rounded-3xl shadow-2xl p-8"
-      >
-        {/* Brand */}
-
+      <div className="relative w-full max-w-md bg-white/10 backdrop-blur-xl border border-white/20 rounded-3xl shadow-2xl p-8">
         <div className="text-center mb-8">
-          <div
-            className="
-        mx-auto mb-4
-        w-14 h-14
-        rounded-2xl
-        bg-blue-600
-        flex items-center justify-center
-        text-white text-2xl
-        font-bold
-        shadow-lg
-        "
-          > 
-            
-      <div className="inline-block bg-blue-200 p-2 rounded-lg">
-      <Image 
-        src="/images/image2.png"
-        alt="Logo"
-        width={200}
-        height={200}
-      />
-      </div>
-            
+          <div className="mx-auto mb-4 w-14 h-14 rounded-2xl bg-blue-600 flex items-center justify-center text-white text-2xl font-bold shadow-lg ">
+            <div className="inline-block bg-blue-200 p-2 rounded-lg">
+              <Image
+                src="/images/image2.png"
+                alt="Logo"
+                width={200}
+                height={200}
+              />
+            </div>
           </div>
 
           <h1 className="text-3xl font-bold text-white">
@@ -110,20 +89,7 @@ export default function LoginPage() {
           </label>
 
           <input
-            className="
-        mt-2
-        w-full
-        p-3
-        bg-white/10
-        text-white
-        placeholder-gray-400
-        border border-white/20
-        rounded-xl
-        outline-none
-        focus:ring-2
-        focus:ring-blue-500
-        transition
-        "
+            className=" mt-2 w-full p-3  bg-white/10  text-white  placeholder-gray-400 border border-white/20 rounded-xl outline-none focus:ring-2  focus:ring-blue-500 transition "
             id="password"
             type="password"
             value={user.password}
@@ -137,12 +103,7 @@ export default function LoginPage() {
         <button
           disabled={buttonDisabled}
           onClick={onLogin}
-          className={`
-      w-full py-3
-      rounded-xl
-      font-semibold
-      text-white
-      transition
+          className={` w-full py-3 rounded-xl font-semibold  text-white transition
 
       ${
         buttonDisabled
@@ -159,9 +120,7 @@ export default function LoginPage() {
 
         <p className="text-center text-gray-300 mt-6">
           Don't have an account?
-          <Link
-            href="/signup"
-            className=" text-blue-400 ml-2 hover:underline">
+          <Link href="/signup" className=" text-blue-400 ml-2 hover:underline">
             Create account
           </Link>
         </p>
